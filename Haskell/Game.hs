3a4ab2762adcbe5b737a2ef229777f8336bc9de1
andrewgph <108860+andrewgph@users.noreply.github.com>
2011-04-27 11:51:43 -0700

    Initial commit of haskell rewrite, removed Connect Four code and redesigned Game class

--- Haskell/Game.hs
+++ Haskell/Game.hs
@@ -1,18 +1,16 @@
 module Game (Player,
+	     playerOne,
+	     playerTwo,
              Game,
-             Move,
-             humanPlayer,
-             makeMove,
-             moves,
+             switchPlayer,
+	     initGame,
+	     currentPlayer,
+	     getPlayerMove,
              terminal,
              checkWin,
-             play,
-             eval,
-             getLines,
-             switch, 
-             maxPlayer,
-             evaluation,
-             getMove) where
+	     nextStates,
+	     minimax,
+	     playerOneTurn) where
 
 import Maybe (isJust)
 
@@ -20,46 +18,55 @@ import Maybe (isJust)
 data Player = One | Two
     deriving (Eq)
 
+playerOne :: Player
+playerOne = One
+
+playerTwo :: Player
+playerTwo = Two
+
 instance Show Player where
     show One = "1"
     show Two = "2"
 
--- |switch returns the opposite player value from its argument, this function is useful for switching players between max and min when calculating the minimax value of a state.
-switch :: Player -> Player 
-switch One = Two
-switch Two = One
+-- |switchPlayer returns the opposite player value from its argument, this function is useful for switching players between max and min when calculating the minimax value of a state.
+switchPlayer :: Player -> Player 
+switchPlayer One = Two
+switchPlayer Two = One
 
 -- |maxPlayer checks whether a player is the max player or not. Player One is max by default.
 maxPlayer :: Player -> Bool
 maxPlayer = (== One)
 
--- |Player One is the human player (in PlayGame.hs) by default.
-humanPlayer :: Player 
-humanPlayer = One
-
--- |The Move type has a type parameter a (a is restricted to an instance of the Game class). A move is a function which takes a Player and returns a new game state. Note that this type can only represent moves in symmetrical games, where a move can be played by either player.
-newtype (Game a) => Move a = Move (Player -> a)
-
--- |makeMove creates a value of type Move when given a function which takes a Player and returns a game.
-makeMove :: (Game a) => (Player -> a) -> Move a
-makeMove f = Move f 
-
--- |The Game class provides an interface for two-player games played on an (n*m) grid. The minimax and alphabeta functions should use this interface to determine the minimax value of a game state.
+-- |The Game class provides an interface for two-player games.
 class Game a where
-    play :: Player -> Move a -> a -- ^ play takes a Player and a Move and returns the game resulting from that player playing that move. It is defined by default.
-    play plyr (Move mv) = mv plyr
+    initGame :: a
+    currentPlayer :: a -> Player
+    getPlayerMove :: a -> IO a
     terminal :: a -> Bool -- ^ A game is terminal (finished) either if one of the players has won, or if there are no moves available. It is defined by default using checkWin and moves.
-    terminal game = isJust (checkWin game) || null (moves game) 
+    terminal game = isJust (checkWin game) || null (nextStates game) 
     checkWin :: a -> Maybe Player -- ^ If either player has won then checkWin returns Just that player, otherwise it returns Nothing.
-    evaluation :: (Num b, Ord b) => a -> b -- ^ The evaluation function gives a ranking for game states, with a positive ranking being good for One and a negative ranking being good for Two.
-    moves :: a -> [Move a] -- ^ moves returns the possible moves available in a game state.
-    getLines :: a -> [[((Int,Int),Maybe Player)]] -- ^ getLines takes a game and returns a list of the grid lines and their status. Note that not all grid lines may be returned, only those that are relevant to the goals of the game. For example, in connect four the grid lines with a length less that three are not relevant to the game.
-    getMove :: a -> IO (Move a) -- ^ getMove gets a move from a human player, you should not need to use this function (unless you are planning on creating your own instances of the Game class).
+    nextStates :: a -> [a] -- ^ nextStates returns the possible next game states from a game state
+    minimax :: a -> Int
+    minimax = minimax'
+    playerOneTurn :: a -> Bool
 
--- |eval provides a default evaluation function for an instance of the Game class. There are three possible evaluations: positive one if player One wins, zero if it is a draw, and negative one if player Two wins. You should use this function when creating the minimax and alphabeta functions that don't have a cut-off test.
-eval :: Game a => a -> Int
-eval game = case (checkWin game) of
-              Just One -> 1
-              Just Two -> (-1)
-              Nothing -> 0
-
+minimax' game | playerOneTurn game = maxValue game (-2,2) 
+              | otherwise    = minValue game (-2,2)
+    where maxValue g (a,b) | terminal g = eval g
+                           | otherwise = snd $ foldr (\g' (a',v) -> 
+                                       let newV = max v (minValue g' (a',b)) in
+                                       if v >= b 
+                                       then (a',v) -- If a state with an evaluation higher than beta has been found, we don't bother checking any further nodes on this branch.
+                                       else (max a' newV, newV)) -- If not, we calculate the new alpha and v values.
+                                (a,-2) (nextStates g)
+          minValue g (a,b) | terminal g = eval g
+                           | otherwise = snd $ foldr (\g' (b',v) ->
+                                       let newV = min v (maxValue g' (a,b')) in
+                                       if v <= a
+                                       then (b',v) -- If a state with an evaluation lower than alpha has been found, ignore the rest of the nodes on this branch.
+                                       else (min b' newV, newV)) -- If not, calculate the new beta and v values.
+                                (b,2) (nextStates g)
+	  eval g = case (checkWin g) of
+			(Just One) -> 1
+			(Just Two) -> -1
+			(_) -> 0
--- Haskell/TicTacToe.hs
+++ Haskell/TicTacToe.hs
@@ -1,52 +1,62 @@
-module TicTacToe (TicTacToe, 
-                          moves, 
-                          checkWin,
-                          evaluation,
-                          getLines,
-                          initNC,
-                          getMove) where
+module TicTacToe (TicTacToeGame,
+		      currentPlayer,
+                      initGameTTT,
+                      checkWin,
+		      getPlayerMove,
+		      nextStates,
+		      playerOneTurn) where
 
 import Array
 import Maybe (isNothing,isJust)
 import Game
 import Grid
-import SearchFns (ncEval)
 
-newtype TicTacToe = NC (Array (Int,Int) (Maybe Player))
+data TicTacToeGame = TicTacToeGame {player :: Player
+				   ,board :: Array (Int,Int) (Maybe Player)}
 
-initNC :: TicTacToe
-initNC = NC $ array ((1,1),(3,3)) [((i,j),Nothing)|i<-[1..3],j<-[1..3]]
+instance Game TicTacToeGame where
+    initGame = initGameTTT
+    checkWin = checkWinTTT
+    getPlayerMove  = getPlayerMoveTTT
+    nextStates = nextStatesTTT
+    playerOneTurn = playerOneTurnTTT 
+    currentPlayer = player
 
-instance Game TicTacToe where
-    moves (NC game) = map (\i -> makeMove (\player -> NC $ game//[(i,Just player)])) indexes
-                       where indexes = map fst $ filter (isNothing.snd) $ assocs game
-    checkWin = checkWinNC
-    evaluation = ncEval
-    getLines (NC game)= map (map (\pt -> (pt, game!pt))) ncLines 
-    getMove game = getMoveNC game
+playerOneTurnTTT :: TicTacToeGame -> Bool
+playerOneTurnTTT game = (player game) == playerOne
 
-getMoveNC :: TicTacToe -> IO (Move TicTacToe)
-getMoveNC (NC game) = do 
+initGameTTT :: TicTacToeGame
+initGameTTT = TicTacToeGame playerOne (array ((1,1),(3,3)) [((i,j),Nothing)|i<-[1..3],j<-[1..3]])
+
+nextStatesTTT :: TicTacToeGame -> [TicTacToeGame]
+nextStatesTTT game = map createGame indices
+	where createGame i = TicTacToeGame (switchPlayer (player game)) ((board game)//[(i,Just (player game))])
+	      indices = map fst $ filter (isNothing.snd) $ assocs $ board game
+
+getPlayerMoveTTT :: TicTacToeGame -> IO TicTacToeGame
+getPlayerMoveTTT game = do 
   putStrLn "Please enter a move (row,column):"
-  getPair >>= checkMove
-      where checkMove mv | not $ null $ filter (\((i,j),x) -> (i,j) == mv && isNothing x) $ assocs game = return $ makeMove (\player -> NC $ game//[(mv,Just player)])
-                         | otherwise = do putStrLn message
-                                          getMove (NC game)
+  getPair >>= getMove
+      where getMove mv | checkMove mv = return $ TicTacToeGame (switchPlayer (player game)) ((board game)//[(mv,Just (player game))])
+                       | otherwise = do putStrLn message
+                                        getPlayerMoveTTT game
+            checkMove mv = not $ null $ filter (\((i,j),x) -> (i,j) == mv && isNothing x) $ assocs $ board game
             message = "That was not a valid move ... please try again."
             ex = \e -> do putStrLn message
                           getPair
             getPair = catch ((readLn)::IO (Int,Int)) ex
      
-ncLines = [[(1,1),(2,2),(3,3)],[(3,1),(2,2),(1,3)]] ++  
+tttLines = [[(1,1),(2,2),(3,3)],[(3,1),(2,2),(1,3)]] ++
           [[(i,j) | i<-[1..3]] | j <- [1..3]] ++ 
           [[(i,j) | j <- [1..3]] | i <- [1..3]]
 
-checkWinNC :: TicTacToe -> Maybe Player
-checkWinNC (NC board) = foldr (\x y -> if isJust x then x else y) Nothing $ map line $ getLines (NC board)
-    where line xs = foldl1 (\x y -> if x /= y then Nothing else x) $ map snd xs
+checkWinTTT :: TicTacToeGame -> Maybe Player
+checkWinTTT game = foldr (\x y -> if isJust x then x else y) Nothing $ map line $ getBoardLines $ board game
+    where line xs = foldl1 (\x y -> if x /= y then Nothing else x) xs
+	  getBoardLines board = map (\l -> map (\s -> board!s) l) tttLines
  
-showNC :: TicTacToe -> String
-showNC (NC x) = showGrid 3 3 x
+showTTT :: TicTacToeGame -> String
+showTTT game = showGrid 3 3 (board game)
 
-instance Show TicTacToe where
-    show = showNC
+instance Show TicTacToeGame where
+    show = showTTT
